c4f03cc6e5743128f59a3c722d63a314a8d3b804
Isakwasser <wasser18@bk.ru>
2023-01-23 21:01:42 +0300

    fix 'prod' branch for develop

---
+++ build/static/js/399.1d407cef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkp1209=self.webpackChunkp1209||[]).push([[399],{6399:function(u,e,n){n.r(e);var t=n(184);e.default=function(){return(0,t.jsx)("h1",{children:"About"})}}}]);
+//# sourceMappingURL=399.1d407cef.chunk.js.map
